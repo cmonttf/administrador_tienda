de561ca114cece1f4ef562859e566dcd51a9e965
cmonttf <cmonttf@gmail.com>
2025-11-08 19:58:34 -0300

    feat: se agrega ruta en api, y redireccionadores de ruta raiz

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get("/", function () {
+    return redirect("/v1");
+});
+
 Route::prefix('v1')->group(function () {
-    Route::resource('producto', ProductoController::class);
+    Route::get("/", function () {
+        return abort(403, "No tiene permiso");
+    });
+    Route::resource('productos', ProductoController::class);
 });
