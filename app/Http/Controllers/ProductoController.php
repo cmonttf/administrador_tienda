22c834978ee029cb13fda1f0ea4b138ef99cbc69
cmonttf <cmonttf@gmail.com>
2025-11-08 22:01:56 -0300

    feat: se agrega el metodo index para enlistar los productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ProductoService;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use StdJsonResponse\JSONResponse;
+use StdJsonResponse\StdResponse;
 
 /**
  * Class ProductoController
@@ -14,12 +19,28 @@ use Illuminate\Http\Request;
  */
 class ProductoController extends Controller
 {
+    public function __construct(
+        private ProductoService $productoService
+    )
+    {}
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $response = new StdResponse("Listado de los productos.", true);
+
+        try {
+            $response->data = $this->productoService->obtenerListadoProductos();
+        } catch (Exception $e) {
+            $response->status = false;
+            $response->message = "Error inesperado: {$e->getMessage()}";
+            Log::error($response->message);
+            JSONResponse::error($response);
+        }
+
+        JSONResponse::success($response);
     }
 
     /**
