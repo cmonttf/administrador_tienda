bf58b2f5d54ffbe388123b653f093e8111c4e60c
cmonttf <cmonttf@gmail.com>
2025-11-22 10:47:01 -0300

    feat: se agrega eliminacion del prefijo api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,9 +7,10 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
-        web: __DIR__.'/../routes/web.php',
+        //web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: ''
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
