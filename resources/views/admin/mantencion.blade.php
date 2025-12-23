0f7dabeafee9987f8c0e7cf21260e474d7a48fd7
cmonttf <cmonttf@gmail.com>
2025-12-23 11:23:13 -0300

    feat: se agrega vista para mostrar que el sitio o la pagina esta en mantencion

---
+++ resources/views/admin/mantencion.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin')
+
+@section('title', 'Mantenimiento')
+@section('page-title', 'Fuera de Servicio')
+
+@section('content')
+
+<div class="bg-white shadow overflow-hidden sm:rounded-lg container">
+    <h1>Mantenimiento</h1>
+    <div class="alert alert-danger" role="alert">
+        Fuera de Servicio!
+    </div>
+</div>
+@endsection
