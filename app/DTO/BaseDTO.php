b6f5a8bb5b9c6541218c106911239e47ab6faf05
cmonttf <cmonttf@gmail.com>
2026-05-18 22:24:17 -0400

    feat: se agrega validacion de campos obligatorios para un objeto

--- app/DTO/BaseDTO.php
+++ app/DTO/BaseDTO.php
@@ -14,4 +14,13 @@ abstract class BaseDTO
         return $datos[$clave];
     }
 
+    public static function campoObligatorioObjeto(object $dato, string $clave, ?string $mensaje = null): mixed
+    {
+        if (!property_exists($dato, $clave)) {
+            $mensaje .= "El campo {$clave} es obligatorio.";
+            throw new \Exception($mensaje);
+        }
+
+        return $dato->{$clave};
+    }
 }
