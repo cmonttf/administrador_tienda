a082f77ae5e57193ecb848bc2504ea8b500a9d14
cmonttf <cmonttf@gmail.com>
2026-05-20 01:02:52 -0400

    feat: se agregan rutas para actualizar y eliminar producto

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ Route::prefix('v1')->group(function () {
         Route::post('/productos', [ProductoController::class, 'store'])->name('products.store');
         Route::get('/productos/show/{id}', [ProductoController::class, 'show'])->name('products.show');
         Route::get('/productos/edit/{id}', [ProductoController::class, 'edit'])->name('products.edit');
-        Route::delete('/productos/destroy/{id}', [ProductoController::class, 'show'])->name('products.destroy');
+        Route::post('/productos/{id}', [ProductoController::class, 'update'])->name('products.update');
+        Route::delete('/productos/destroy/{id}', [ProductoController::class, 'destroy'])->name('products.destroy');
     });
 });
