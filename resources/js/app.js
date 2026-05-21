6b8d762809c3661959115f585fc396427cc2988a
cmonttf <cmonttf@gmail.com>
2026-05-21 00:57:01 -0400

    feat: se agrega inicializador de data table

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+
+import $ from 'jquery';
+
+window.$ = window.jQuery = $;
+
+import DataTable from 'datatables.net-dt';
+import 'datatables.net-dt/css/dataTables.dataTables.css';
+
+$(document).ready(function () {
+    $('#tabla').DataTable({
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/2.0.8/i18n/es-CL.json'
+        }
+    });
+});
