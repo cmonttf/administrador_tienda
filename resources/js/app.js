2e68f518f0a4629092308493294078162bc7c940
cmonttf <cmonttf@gmail.com>
2026-05-22 17:59:41 -0400

    feat: se agrega componentes de vue en app.js para la carga de datos en tiempo real

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,66 @@
 import './bootstrap';
 
+/*
+|--------------------------------------------------------------------------
+| JQUERY
+|--------------------------------------------------------------------------
+*/
+
 import $ from 'jquery';
 
 window.$ = window.jQuery = $;
 
+/*
+|--------------------------------------------------------------------------
+| DATATABLES
+|--------------------------------------------------------------------------
+*/
+
 import DataTable from 'datatables.net-dt';
+
 import 'datatables.net-dt/css/dataTables.dataTables.css';
 
+/*
+|--------------------------------------------------------------------------
+| VUE
+|--------------------------------------------------------------------------
+*/
+
+import { createApp } from 'vue';
+
+import Dashboard from './components/Dashboard.vue';
+
+/*
+|--------------------------------------------------------------------------
+| MONTAR VUE
+|--------------------------------------------------------------------------
+*/
+
+if (document.getElementById('dashboard-app')) {
+
+    createApp(Dashboard)
+        .mount('#dashboard-app');
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| DATATABLES INIT
+|--------------------------------------------------------------------------
+*/
+
 $(document).ready(function () {
-    $('#tabla').DataTable({
-        language: {
-            url: 'https://cdn.datatables.net/plug-ins/2.0.8/i18n/es-CL.json'
-        }
-    });
+
+    if ($('#tabla').length) {
+
+        $('#tabla').DataTable({
+
+            language: {
+                url: 'https://cdn.datatables.net/plug-ins/2.0.8/i18n/es-CL.json'
+            }
+
+        });
+
+    }
+
 });
