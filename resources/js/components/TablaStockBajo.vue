c3df0bb5c6afb8a5251245cf44ddd0f73ee0fc4d
cmonttf <cmonttf@gmail.com>
2026-05-22 22:11:36 -0400

    feat: se agrega componente para crear una tabla con stocks bajo

---
+++ resources/js/components/TablaStockBajo.vue
@@ -0,0 +1,82 @@
+<template>
+
+    <div class="tabla-container">
+
+        <table class="tabla-stock">
+
+            <thead>
+
+                <tr>
+
+                    <th>Producto</th>
+                    <th>Stock</th>
+
+                </tr>
+
+            </thead>
+
+            <tbody>
+
+                <!-- LISTADO -->
+
+                <tr
+                    v-for="producto in productos"
+                    :key="producto.nombre"
+                >
+
+                    <td>
+                        {{ producto.nombre }}
+                    </td>
+
+                    <td>
+                        {{ producto.cantidad }}
+                    </td>
+
+                </tr>
+
+                <!-- SIN DATOS -->
+
+                <tr
+                    v-if="productos.length === 0"
+                >
+
+                    <td
+                        colspan="2"
+                        class="sin-datos"
+                    >
+
+                        No hay productos con stock bajo
+
+                    </td>
+
+                </tr>
+
+            </tbody>
+
+        </table>
+
+    </div>
+
+</template>
+
+<script setup>
+
+/*
+|--------------------------------------------------------------------------
+| PROPS
+|--------------------------------------------------------------------------
+*/
+
+defineProps({
+
+    productos: {
+
+        type: Array,
+
+        required: true
+
+    }
+
+});
+
+</script>
