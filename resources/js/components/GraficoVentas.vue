d658ff47476e660e3cc81853fd12adf746ab7c16
cmonttf <cmonttf@gmail.com>
2026-05-22 22:01:57 -0400

    feat: se agrega componente para crear graficos reutilizable

---
+++ resources/js/components/GraficoVentas.vue
@@ -0,0 +1,48 @@
+<template>
+
+    <div class="grafico">
+
+        <apexchart
+            type="line"
+            height="350"
+            :options="chartOptions"
+            :series="series"
+        />
+
+    </div>
+
+</template>
+
+<script setup>
+
+/*
+|--------------------------------------------------------------------------
+| PROPS
+|--------------------------------------------------------------------------
+*/
+
+defineProps({
+
+    series: {
+        type: Array,
+        required: true
+    },
+
+    chartOptions: {
+        type: Object,
+        required: true
+    }
+
+});
+
+</script>
+
+<style scoped>
+
+.grafico {
+
+    width: 100%;
+
+}
+
+</style>
