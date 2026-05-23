ea13d07842f4bcf867f1bd5a01e2a8f96acdc808
cmonttf <cmonttf@gmail.com>
2026-05-22 22:37:53 -0400

    feat: se agrega interface para obtener la cantidad total de productos

--- app/Interfaces/DashboardInterface.php
+++ app/Interfaces/DashboardInterface.php
@@ -5,4 +5,6 @@ namespace App\Interfaces;
 interface DashboardInterface
 {
     public static function obtenerProductosQueSeAcaban(): array;
+
+    public static function obtenerTotalProducto(): int;
 }
