5f88d1a1ff16f41391670fa3c003a1779fff8d81
cmonttf <cmonttf@gmail.com>
2026-05-22 22:29:49 -0400

    feat: se modifica archivo para traer las rutas de vistas

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,33 @@
 <?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get("/", function () {
+    return redirect("/v1");
+});
+
+Route::prefix('v1')->group(function () {
+    Route::get('/', function () {
+        return view('admin.mantencion'); // o redirect('/v1/admin/dashboard')
+    })->name('admin.mantencion');
+
+    //Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::get('/productos', [ProductoController::class, 'index'])->name('products.index');
+        Route::get('/productos/create', [ProductoController::class, 'create'])->name('products.create');
+        Route::post('/productos', [ProductoController::class, 'store'])->name('products.store');
+        Route::get('/productos/show/{id}', [ProductoController::class, 'show'])->name('products.show');
+        Route::get('/productos/edit/{id}', [ProductoController::class, 'edit'])->name('products.edit');
+        Route::post('/productos/{id}', [ProductoController::class, 'update'])->name('products.update');
+        Route::delete('/productos/destroy/{id}', [ProductoController::class, 'destroy'])->name('products.destroy');
+    });
+});
